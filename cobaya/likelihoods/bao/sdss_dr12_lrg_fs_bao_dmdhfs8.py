778283bd68b535c947f1ec0b93f8010efcfb9a87
Angelo Ferrari <anferrar@bo.infn.it>
2024-06-09 00:12:19 +0200

    Add low z BAO + RSD likelihoods from SDSS to combine with DESI

---
+++ cobaya/likelihoods/bao/sdss_dr12_lrg_fs_bao_dmdhfs8.py
@@ -0,0 +1,5 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_dr12_lrg_fs_bao_dmdhfs8(BAO):
+    pass
---
+++ cobaya/likelihoods/bao/sdss_mgs_fs.py
@@ -0,0 +1,5 @@
+from cobaya.likelihoods.base_classes import BAO
+
+
+class sdss_mgs_fs(BAO):
+    pass
